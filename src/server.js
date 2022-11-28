6841dbe2112f49cd201215a014a7146175925bf5
OD <kg-te@naver.com>
2022-11-28 19:38:22 +0900

    :tada: 프로젝트 초기화 & 기본 의존성 패키지 설치 & 초기 설정

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+
+const handleListen = () => console.log(`Listening on http://localhost:8800`);
+app.listen(8800, handleListen);
